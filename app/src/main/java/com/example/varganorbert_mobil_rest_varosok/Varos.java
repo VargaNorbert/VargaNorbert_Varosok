04512eecda53376f791a256ed40876ea8c9296db
Varga Norbert <vnorbi02@gmail.com>
2022-11-25 20:11:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/varganorbert_mobil_rest_varosok/Varos.java
@@ -0,0 +1,51 @@
+package com.example.varganorbert_mobil_rest_varosok;
+
+public class Varos {
+    private int id;
+    private String nev;
+    private String orszag;
+    private int lakossag;
+
+    public Varos(int id, String nev, String orszag, int lakossag) {
+        this.id = id;
+        this.nev = nev;
+        this.orszag = orszag;
+        this.lakossag = lakossag;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public String getOrszag() {
+        return orszag;
+    }
+
+    public void setOrszag(String orszag) {
+        this.orszag = orszag;
+    }
+
+    public int getLakossag() {
+        return lakossag;
+    }
+
+    public void setLakossag(int lakossag) {
+        this.lakossag = lakossag;
+    }
+
+    @Override
+    public String toString() {
+        return "Város: { " +
+                "Név" + ":" + nev + "\n" +
+                "Ország" + ":" + orszag + "\n" +
+                "Lakosság" + ":" + lakossag + "}";
+    }
+}
