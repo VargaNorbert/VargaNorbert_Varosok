cb61f927b469cf09964f0325f025a0622bf63d21
Varga Norbert <vnorbi02@gmail.com>
2022-11-25 20:36:10 +0100

    MainActivity gombok

--- app/src/main/java/com/example/varganorbert_mobil_rest_varosok/MainActivity.java
+++ app/src/main/java/com/example/varganorbert_mobil_rest_varosok/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.varganorbert_mobil_rest_varosok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button listButton;
+    private Button addButton;
+
+    public void init(){
+        addButton = findViewById(R.id.addButton);
+        listButton = findViewById(R.id.listButton);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InsertActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
+
 }
